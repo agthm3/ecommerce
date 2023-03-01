ac402743ee43f0c712c4b81c1731e5a0ca2252a7
Andi Gigatera Halil M <54834402+agthm3@users.noreply.github.com>
2023-03-01 13:54:01 +0800

    fixing logic, when new cart has same cart before or existing cart, update it.

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -16,22 +16,34 @@ class CartController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function add_to_cart(Request $request, Product $product){
-        $request->validate([
-            'amount' => 'required|gte:1|lte:'.$product->stock,
-        ]);
-
-        
         $user_id = Auth::id();
         $product_id = $product->id;
+        $existing_cart = Cart::where('product_id', $product_id)
+            ->where('user_id', $user_id)->first();
+
+        if($existing_cart == null){
+            $request->validate([
+                'amount' => 'required|gte:1|lte:'.$product->stock,
+            ]);
+            Cart::create([
+                'user_id'=> $user_id, 
+                'product_id' => $product_id, 
+                'amount' => $request->amount
+            ]);
+        } else{
+            $request->validate([
+                    'amount' => 'required|gte:1|lte:'.($product->stock- $existing_cart->amount),
+                ]);
+            $existing_cart->update([
+                'amount' => $existing_cart->amount + $request->amount
+            ]);
+        }
 
 
-        Cart::create([
-            'user_id'=> $user_id, 
-            'product_id' => $product_id, 
-            'amount' => $request->amount
-        ]);
 
-        return Redirect::route('index_product');
+
+
+        return Redirect::route('show_cart');
     }
     public function index()
     {

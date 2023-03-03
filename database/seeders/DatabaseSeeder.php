f3e0bdab3ffce52f53f00c27215586a01b0c4563
Andi Gigatera Halil M <54834402+agthm3@users.noreply.github.com>
2023-03-03 20:31:45 +0800

    admin seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Config;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +23,15 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+
+        DB::table('users')->insert([
+            'id' => 1, 
+            'name' => 'Admin', 
+            'email' => 'gigam57@gmail.com', 
+            'password' => Hash::make('ROMAWI123'), 
+            'is_admin' => true
+        ]);
     }
+
 }
